fac8d088582ab11fa84068a680231596118add0a
Noah Smith <noahcsmith1993@gmail.com>
2023-09-18 09:53:25 -0400

    initial commit, adds project and comment models and related methods

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
+from datetime import timezone
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+# Project model
+# One to many with comments (project -|--< comments)
+# One to many with users (project >--|- user)
+class Project(models.Model):
+    title = models.CharField(max_length=20)
+    repository = models.CharField(max_length=200)
+    deployment = models.CharField(max_length=200)
+    thumbnail = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'project_id': self.id})
+
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=250)
+    timestamp = models.DateTimeField(default=timezone.now)
+    project = models.ForeignKey(Project)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment with ID {self.id} made by User ID {self.user} on Project ID {self.project}"
+    
+    class Meta:
+        ordering = ['-date']
