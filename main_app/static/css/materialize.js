1ab4c9b6db50177c02d2d98dfda453addf0b96fc
Jordon <jordonmeril9@gmail.com>
2023-09-19 09:26:17 -0400

    Adds the favicon icon adds, addition templates and js

--- main_app/static/css/materialize.js
+++ main_app/static/css/materialize.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
   });
-  
