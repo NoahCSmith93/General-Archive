a4eed33a98c6554c332331e97f5ce045a5caea4c
Noah Smith <noahcsmith1993@gmail.com>
2023-09-21 13:01:31 -0400

    Commit 7

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
-echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
 pip install django
-# pip install dj-database-url
-# pip install django-environ
-# pip install gunicorn
-# pip install psycopg2-binary
-# pip install whitenoise
+pip install dj-database-url
+pip install django-environ
+pip install gunicorn
+pip install psycopg2-binary
+pip install whitenoise
 
 python manage.py collectstatic --no-input
 python manage.py migrate
