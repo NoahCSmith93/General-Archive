e994a8e1ebba79a2dfbf283ff322d0f6167b97e4
Noah Smith <noahcsmith1993@gmail.com>
2023-09-21 12:02:03 -0400

    Further modifies build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 pip install -r requirements.txt
 
 pip install django
+pip install dj-database-url
+pip install django-environ
+pip install gunicorn
+pip install psycopg2-binary
+pip install whitenoise
+
 python manage.py collectstatic --no-input
 python manage.py migrate
