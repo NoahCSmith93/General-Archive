ec60bd27496036aaa9482514183f957f24d0469e
Noah Smith <noahcsmith1993@gmail.com>
2023-09-21 12:31:00 -0400

    Commit 3 to resolve deployment issues

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
+pip install --upgrade pip
 pip install -r requirements.txt
 
-pip install django
-pip install dj-database-url
-pip install django-environ
-pip install gunicorn
-pip install psycopg2-binary
-pip install whitenoise
+# pip install django
+# pip install dj-database-url
+# pip install django-environ
+# pip install gunicorn
+# pip install psycopg2-binary
+# pip install whitenoise
 
 python manage.py collectstatic --no-input
 python manage.py migrate
